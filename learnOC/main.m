4e0af30b289b58b0ed858dc933c226152c11fa4a
Juude <juude.song@gmail.com>
2017-01-27 16:11:22 +0800

    Initial Commit

---
+++ learnOC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  learnOC
+//
+//  Created by ruoyi.sjd on 2017/1/27.
+//  Copyright © 2017年 ruoyi.sjd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
